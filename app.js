74ea2e6dacacea13db0644575dbaaf9e8d6286f1
Tarun Singh Tanwar <tarunsinghtanwar14@gmail.com>
2021-07-25 14:37:05 +0530

    added error handling to all the routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const db = mongoose.connection;
 const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
+const catchAsync=require('./utils/catchAsync');
 mongoose
 	.connect('mongodb://localhost:27017/camper', { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => {
@@ -23,59 +24,56 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 //index page 
-app.get('/campgrounds',async (req,res)=>{
+app.get('/campgrounds', catchAsync(async (req,res)=>{
 	const camps= await Campground.find({});
     res.render('campgrounds/home',{camps});
-})
+}))
 
 //create new
 app.get('/campgrounds/new', (req, res) => {
 	res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async (req, res,next) => {
-	try{
+app.post('/campgrounds', catchAsync(async (req, res,next) => {
+	
 	const camp = new Campground(req.body);
 	await camp.save();
 	res.redirect(`/campgrounds/${camp._id}`)
-	}
-	catch(e){
-		next(e);
-	}
-})
-
+	
+	
+}))
 
 // details page
 
-app.get('/campgrounds/:id',async (req,res)=>{
+app.get('/campgrounds/:id', catchAsync(async (req,res)=>{
 	const { id } = req.params;
 	const camp= await Campground.findById(id);
 res.render('campgrounds/details',{id,camp});
-})
+}))
 
 
 //edit
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 	const { id } = req.params;
 	const camp = await Campground.findById(id);
 	res.render('campgrounds/edit', { camp })
-})
+}))
 
-app.put('/campgrounds/:id',async (req,res)=>{
+app.put('/campgrounds/:id', catchAsync(async (req,res)=>{
 	const { id } = req.params;
 	const camp = await Campground.findByIdAndUpdate(id, { ...req.body});
 	res.redirect(`/campgrounds/${camp._id}`)
-})
+}))
 //delete
 
-app.delete('/campgrounds/:id',async (req,res)=>{
+app.delete('/campgrounds/:id', catchAsync(async (req,res)=>{
 	const {id } = req.params;
 	await Campground.findByIdAndDelete(id);
 	res.redirect('/campgrounds');
-})
+}))
 
-
+//this is to throw errors
 app.use((err,req,res,next)=>{
 	res.send("Something went wrong!");
 })
