b83980b34565ee37210a59ddb38400d8adf254c4
tarunsinghtanwar <tarunsinghtanwar14@gmail.com>
2021-07-28 13:18:23 +0530

    added review model to all campgrounds

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const Campground = require('./models/campground');
 const ejsMate = require('ejs-mate');
 const catchAsync=require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const { campgroundSchema } = require('./schemas.js');
+const { campgroundSchema,reviewSchema } = require('./schemas.js');
+const Review=require('./models/review.js');
 mongoose
 	.connect('mongodb://localhost:27017/camper', { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => {
@@ -24,9 +25,19 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
+//validating server side so that no one can request from postman and all something that is broken
 const validateCampground = (req, res, next) => {
 	const { error } = campgroundSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map(el => el.message).join(',')
+		throw new ExpressError(msg, 400)
+	} else {
+		next();
+	}
+}
+
+const validateReview = (req, res, next) => {
+	const { error } = reviewSchema.validate(req.body);
 	if (error) {
 		const msg = error.details.map(el => el.message).join(',')
 		throw new ExpressError(msg, 400)
@@ -60,7 +71,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res,next) =>
 
 app.get('/campgrounds/:id', catchAsync(async (req,res)=>{
 	const { id } = req.params;
-	const camp= await Campground.findById(id);
+	const camp= await Campground.findById(id).populate('reviews');    //populating reviews which had only objectid stored in campground
 res.render('campgrounds/details',{id,camp});
 }))
 
@@ -85,6 +96,26 @@ app.delete('/campgrounds/:id', catchAsync(async (req,res)=>{
 	await Campground.findByIdAndDelete(id);
 	res.redirect('/campgrounds');
 }))
+
+//Review post
+app.post('/campgrounds/:id/reviews',validateReview, catchAsync(async (req,res)=>{
+	const { id } = req.params;
+	const camp = await Campground.findById(id);
+	const review=new Review(req.body.review);
+	camp.reviews.push(review);
+	await review.save();
+	await camp.save();
+	res.redirect(`/campgrounds/${camp._id}`);
+}))
+//delete review
+app.delete('/campgrounds/:id/reviews/:reviewId',catchAsync(async (req,res)=>{
+	const {id,reviewId}=req.params;
+	await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
+	await Review.findByIdAndDelete(reviewId);
+	res.redirect(`/campgrounds/${id}`)
+}))
+
+
 app.all('*',(req,res,next)=>{
 	next(new ExpressError('Page not found',404));
 })
--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,27 @@
 const mongoose = require('mongoose');
-const schema=mongoose.Schema;
-
-const CamgroundSchema =new schema({
+const Schema=mongoose.Schema;
+const Review = require('./review')
+const CampgroundSchema =new Schema({
     title: String,
     image:String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews:[{ 
+        type: Schema.Types.ObjectId,
+        ref:'Review'
+    }]
+})
+//deleting all reviews if a campground is deleted 
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
-module.exports =mongoose.model('Campground',CamgroundSchema);
+
+module.exports =mongoose.model('Campground',CampgroundSchema);
